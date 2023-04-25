b07e1b2a3f4adc0390d74c76fa6d54d2d46d0eb9
ugly-ceaser <martins.paraclet@yahoo.com>
2023-04-24 17:55:06 -0700

    first commit

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int SumSqaure(int firstNum,int secondNum,int thirdNum);
+void fetch(char **arg_vector,char **env_variable);
+
+
+
+#endif // SHELL_H
