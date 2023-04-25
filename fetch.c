f790fe50bdc72072f132d20df7346dbc67511d58
ugly-ceaser <martins.paraclet@yahoo.com>
2023-04-25 14:39:44 -0700

    task 2 done

--- fetch.c
+++ fetch.c
@@ -1,6 +1,8 @@
 #include "shell.h"
 #include <sys/wait.h>
 
+#define MaxVAlue 10
+
 /**
  * fetch - read input from the user and execute the command
  * @arg_vector: array of strings containing the command-line arguments
@@ -12,15 +14,10 @@ void fetch(char **arg_vector, char **env_variable)
 {
 
     char *Data = NULL;
-
-    int Checker, Progress_Status;
-
+    int Checker, Progress_Status, Counter;
     size_t Num = 0;
-
     ssize_t Char_Counter;
-
-    char *Arg_vector[] = {NULL, NULL};
-
+    char *Arg_vector[MaxVAlue];
     pid_t Child_Process;
 
     while (1)
@@ -44,13 +41,20 @@ void fetch(char **arg_vector, char **env_variable)
         while (Data[Checker])
         {
             if (Data[Checker] == '\n')
-                    Data[Checker] = 0;
-            
+                Data[Checker] = 0;
             Checker++;
         }
 
+        Counter = 0;
+
         /* Set the command to be executed */
-        Arg_vector[0] = Data;
+        Arg_vector[Counter] = strtok(Data, " ");
+
+        /* Parse the input string into an array of arguments */
+        while (Arg_vector[Counter] != NULL)
+        {
+            Arg_vector[++Counter] = strtok(NULL, " ");
+        }
 
         /* Create a child process to execute the command */
         Child_Process = fork();
@@ -66,11 +70,16 @@ void fetch(char **arg_vector, char **env_variable)
         if (Child_Process == 0)
         {
             if (execve(Arg_vector[0], Arg_vector, env_variable) == -1)
-                    printf("%s :Operation failed\n", arg_vector[0]);
+            {
+                /* If execve fails, print an error message to the console */
+                printf("%s :Operation failed\n", arg_vector[0]);
+            }
         }
         /* If this is the parent process, wait for the child to finish */
         else
+        {
             wait(&Progress_Status);
+        }
     }
 }
 
